675c44b35858bce3c677d8f10162677a0b8e86e7
cathsreign <118703583+cathsreign@users.noreply.github.com>
2022-12-01 18:26:12 -0800

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+import math
+x = int(input("Assign a value to 'x': "))
+y = int(input("Assign a value to 'y': "))
+a = x**y
+
+print("x**y =", a)
+
+b = math.log(x, 2)
+
+print("log2(x) =", b)
